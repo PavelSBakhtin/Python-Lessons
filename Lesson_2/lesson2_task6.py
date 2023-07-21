b6a7e3eed634dcd42643a3d80960ade17da8af3b
PavelSB <1987PavelS@mail.ru>
2023-07-21 18:38:51 +0300

    all tasks of lesson 2 completed

--- Lesson_2/lesson2_task6.py
+++ Lesson_2/lesson2_task6.py
@@ -8,50 +8,65 @@
 # При превышении суммы в 5 млн, вычитать налог на богатство 10% перед каждой операцией, даже ошибочной;
 # Любое действие выводит сумму денег.
 
-balance = 0
-count = 0
-ACTION = 50
-COMMISSION = 0.015
-BONUS = 1.03
-TAX = 1.10
-MIN_FEE = 30
-MAX_FEE = 600
+balance = 0 # баланс
+count = 0 # счётчик пополнени/снияти
+ACTION = 50 # кратность
+COMMISSION = 0.015 # % за снятие
+MIN_FEE = 30 # минимум такса при снятии
+MAX_FEE = 600 # максимум такса при снятии
+BONUS = 1.03 # % на остаток
+LIMIT = 5000000 # порог богатства
+TAX = 0.10 # налог на богатство
 
 while True:
-    print("========== БАНКОМАТ ==========")
-    print("1. ПОПОЛНИТЬ")
-    print("2. СНЯТЬ")
-    print("3. ВЫЙТИ")
-    match int(input("ВЫБЕРИТЕ ДЕЙСТВИЕ:\n ->")):
-        case 1:
-            dep = int(input("ВВЕДИТЕ СУММУ ПОПОЛНЕНИЯ: "))
-            if dep % 50 == 0:
-                if count % 3 == 0:
-                    balance += dep
-                    balance *= BONUS
-                    count += 1
-                    print(f"БАЛАНС СОСТОВЛЯЕТ: {balance} у.е.")
-                else:
-                    balance += dep
-                    count += 1
-                    print(f"БАЛАНС СОСТОВЛЯЕТ: {balance} у.е.")
+    print('\n========== БАНКОМАТ ==========')
+    if balance > LIMIT:
+        balance -= (balance * TAX)
+    print(f'Баланс: {balance} у.е.')
+    print('1. ПОПОЛНИТЬ')
+    print('2. СНЯТЬ')
+    print('3. ВЫЙТИ')
+    menu = int(input('Выберите пункт меню: '))
+    if menu == 1:
+        print('\nПополнение баланса >>>')
+        depo_plus = int(input('Введите сумму пополнения: '))
+        if depo_plus % ACTION == 0:
+            count += 1
+            balance += depo_plus
+        else:
+            print('\nПополнение невозможно, только кратно 50 у.е.')
+        print(f'\nБаланс пополнен на сумму: {depo_plus}')
+        if count % 3 == 0:
+            balance *= BONUS
+            round(balance, 2)
+            print('\nВам начислено 3% на остаток')
+        print(f'\nБаланс: {balance} у.е.')
+    elif menu == 2:
+        print('\nСниятие с баланса >>>')
+        depo_minus = int(input('Введите сумму снятия: '))
+        if depo_minus % ACTION == 0:
+            fee = round(depo_minus * COMMISSION, 2)
+            if fee < MIN_FEE:
+                fee = MIN_FEE
+            elif fee > MAX_FEE:
+                fee = MAX_FEE
+            total = depo_minus + fee
+            if total < balance:
+                count += 1
+                balance -= total
             else:
-                print("ПОПОЛНЕНИЕ ТОЛЬКО КРАТНО 50 у.е.")
-        case 2:
-            withdraw = int(input("CУММА CНЯТИЯ: "))
-            if withdraw % 50 == 0 and withdraw < balance:
-                fee = withdraw * COMMISSION
-                if MIN_FEE < fee < MAX_FEE:
-                    if count % 3 == 0:
-                        balance -= withdraw - fee
-                        balance *= BONUS
-                        count += 1
-                        print(f"БАЛАНС СОСТОВЛЯЕТ: {balance} у.е.")
-                    else:
-                        balance -= withdraw - fee
-                        count += 1
-                        print(f"БАЛАНС СОСТОВЛЯЕТ: {balance} у.е.")
-            else:
-                print("СУММА СНЯТИЯ ПРЕВЫШАЕТ БАЛАНС СЧЕТА!")
-        case 3:
-            exit()
+                print('\nНедостаточно средств')
+        else:
+            print('\nСнятие невозможно, только кратно 50 у.е.')
+        print(f'\nБаланс уменьшен на сумму: {total}')
+        if count % 3 == 0:
+            balance *= BONUS
+            round(balance, 2)
+            print('\nВам начислено 3% на остаток')
+        print(f'\nБаланс: {balance} у.е.')
+    elif menu == 3:
+        print(f'\nБаланс: {balance} у.е.')
+        exit()
+    else:
+        print(f'\nБаланс: {balance} у.е.')
+        print(f'\nНеверно введена команда')
