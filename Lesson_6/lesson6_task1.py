52239faec9c830869cb537c1caeb8b893b8d1131
PavelSB <1987PavelS@mail.ru>
2023-07-27 21:11:23 +0300

    lesson 6 task 1 done

--- Lesson_6/lesson6_task1.py
+++ Lesson_6/lesson6_task1.py
@@ -2,4 +2,15 @@
 # Создайте файл, в котором вы импортируете встроенные
 # в модуль функции под псевдонимами. (3-7 строк импорта).
 
+import random as rnd
 
+a = rnd.randint(0, 10)
+print(a)
+
+from random import randint as rnd_int
+
+b = rnd_int(0, 10)
+print(b)
+
+from sys import * # всё из модуля, не рекомендуется
+import os 
