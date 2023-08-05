fee1e888457f82eafa9776613bd39f54896ca50d
PavelSB <1987PavelS@mail.ru>
2023-08-05 14:33:44 +0300

    lesson 7 task 7 files created

--- Lesson_7/lesson7_task7.py
+++ Lesson_7/lesson7_task7.py
@@ -2,4 +2,18 @@
 # Каждая группа включает файлы с несколькими расширениями.
 # В исходной папке должны остаться только те файлы, которые не подошли для сортировки.
 
+from lesson7_task5 import make_file
+import os
 
+
+def replace_file():
+    for file in os.listdir():
+        exception = file.split('.')[-1]
+        if not os.path.exists(exception):
+            os.mkdir(exception)
+        os.replace(file, os.path.join(os.getcwd(), exception, file))
+
+
+data = {'jpg': 5, 'bmp': 4, 'png': 3}
+make_file(**data)
+# replace_file()
