b3b477b1afdca06909ac4fab85eacc3c426cd629
PavelSB <1987PavelS@mail.ru>
2023-08-05 14:04:44 +0300

    lesson 7 task 6 done

--- Lesson_7/lesson7_task6.py
+++ Lesson_7/lesson7_task6.py
@@ -3,4 +3,15 @@
 # Отсутствие/наличие директории не должно вызывать ошибок в работе функции (добавьте проверки).
 # Существующие файлы не должны удаляться/изменяться в случае совпадения имён.
 
+from lesson7_task4 import create_file
+import os
 
+
+def create_to_path(path, extention):
+    if not os.path.exists(path):
+        os.mkdir(path)
+    os.chdir(path)
+    create_file(extention)
+
+
+create_to_path('lesson7_task6', 'mp3')
