f8a16e7fb58df6274c97c2517badd7665b833175
PavelSB <1987PavelS@mail.ru>
2023-08-09 23:25:29 +0300

    lesson 8 task 7 done

--- Lesson_8/lesson8_task7.py
+++ Lesson_8/lesson8_task7.py
@@ -1,5 +1,29 @@
 # Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader.
 # Распечатайте его как pickle строку.
 
+import csv
+import pickle
 
 
+def csv_to_json(file):
+    with open(file, 'r', newline='') as f:
+        csv_file = csv.reader(f, delimiter=';')
+        count = 0
+        values = []
+        for line in csv_file:
+            if count == 0:
+                graphs = line
+                count += 1
+            else:
+                values.append(line)
+        dict_to_print = {}
+        item = 0
+        for key in graphs:
+            dict_to_print[key] = values[item]
+            item += 1
+    print(f'{dict_to_print}\n')
+    result = pickle.dumps(dict_to_print, protocol=pickle.DEFAULT_PROTOCOL)
+    print(f'{result = }')
+
+
+csv_to_json('lesson8_task6.csv')
