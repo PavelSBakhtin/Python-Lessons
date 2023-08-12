9522265e0ac3a1134cf4d6365c0942575cdfc3ee
PavelSB <1987PavelS@mail.ru>
2023-08-12 20:57:30 +0300

    lesson 9 task 3 done

--- Lesson_9/lesson9_task3.py
+++ Lesson_9/lesson9_task3.py
@@ -5,5 +5,24 @@
 # Для декорирования напишите функцию, которая может принимать как позиционные, так и ключевые аргументы.
 # Имя файла должно совпадать с именем декорируемой функции.
 
+import json
 
 
+def json_saver(func):
+    def wrapper(*args, **kwargs):
+        with open(f'{func.__name__}.json', 'a') as file:
+            temp_dict = {'args': args}
+            temp_dict.update(kwargs)
+            result = func(*args, **kwargs)
+            temp_dict['result'] = result
+            json.dump(temp_dict, file, indent=2, ensure_ascii=False)
+        return result
+    return wrapper
+
+
+@json_saver
+def lesson9_task3(a, b, c='5'):
+    return max(a, b, c)
+
+
+print(lesson9_task3(3, 5, c=7))
