11ad3a3643de4e95ca5f1620c49dcf400df4b57c
PavelSB <1987PavelS@mail.ru>
2023-08-12 23:03:35 +0300

    lesson 9 task 6 done

---
+++ Lesson_9/lesson9_task6.py
@@ -0,0 +1,85 @@
+# Доработайте прошлую задачу добавив декоратор wraps в каждый из декораторов.
+
+from random import randint
+import json
+from functools import wraps
+
+
+def value_control(func):
+    @wraps(func)
+    def wrapper(upper_limit, attempt):
+        if not 0 < upper_limit < 100:
+            upper_limit = randint(1, 100)
+        if not 0 < attempt < 10:
+            attempt = randint(1, 10)
+        func(upper_limit, attempt)
+    return wrapper
+
+
+def json_saver(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with open(f'{func.__name__}.json', 'r') as f1:
+            try:
+                data = json.load(f1)
+            except:
+                data = {1: None}
+        with open(f'{func.__name__}.json', 'w') as f2:
+            if max(data.keys()) != 1:
+                key = int(max(data.keys())) + 1
+            else:
+                key = max(data.keys())
+            temp_dict = {'args': args}
+            temp_dict.update(kwargs)
+            result = func(*args, **kwargs)
+            temp_dict['result'] = result
+            data[key] = temp_dict
+            json.dump(data, f2, indent=2, ensure_ascii=False)
+        return result
+    return wrapper
+
+
+def call_count(num):
+    @wraps(num)
+    def decorator(func):
+        result = []
+        def wrapper(*args, **kwargs):
+            for _ in range(num):
+                result.append(func(*args, **kwargs))
+            return result
+        return wrapper
+    return decorator
+
+
+@call_count(1)
+@value_control
+@json_saver
+def lesson9_task6(upper_limit, attempt):
+    lower_limit = 1
+    number = randint(lower_limit, upper_limit)
+    print('Вы попали в игру - Угадай число!\nУ вас есть {} попыток'.format(attempt))
+    win = False
+
+    while attempt > 0:
+        num = int(input('Введите число от 1 до {}: '.format(upper_limit)))
+        if num > number:
+            print('Загаданное число меньше')
+            attempt -= 1
+        elif num < number:
+            print('Загаданное число больше')
+            attempt -= 1
+        else:
+            print('Загаданное число угадано')
+            win = True
+            break
+        print('Осталось попыток: ', attempt)
+
+    if win:
+        print('Вы выйграли!\nНе использовано попыток: {}'.format(attempt - 1))
+    else:
+        print('Вы проиграли...\nЗагаданное число: {}'.format(number))
+    return win
+
+
+upper_limit, attempt = int(input('Укажите предел: ')),int(input('Количество попыток: '))
+lesson9_task6(upper_limit, attempt)
