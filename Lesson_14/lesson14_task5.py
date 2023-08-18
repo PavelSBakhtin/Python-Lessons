0e03cc72e187ee1d93f3a0cccf2e16599a9d165e
PavelSB <1987PavelS@mail.ru>
2023-08-18 21:40:07 +0300

    lesson 14 tasks 5-6 added

--- Lesson_14/lesson14_task5.py
+++ Lesson_14/lesson14_task5.py
@@ -2,6 +2,67 @@
 # периметр, площадь и позволяющий складывать и вычитать прямоугольники беря за основу периметр.
 # Напишите 3-7 тестов unittest для данного класса.
 
+class Rect:
 
+    def __init__(self, a, b):
+        self.a = a
+        if b is None:
+            self.b = a
+        else:
+            self.b = b
+        self.validate()
+
+    def validate(self):
+        if self.a <= 0 or self.b <= 0:
+            raise ValueError('Ошибка!!! Значение должно быть больше нуля!')
+
+    def __add__(self, other):
+        if isinstance(other, Rect):
+            return Rect(self.a + other.a, self.b + other.b)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Rect):
+            if other.a > self.a or other.b > self.b:
+                raise ValueError('Такой прямоугольник невозможен')
+            return Rect(self.a - other.a, self.b - other.b)
+        return NotImplemented
+
+    def get_area(self):
+        return self.a * self.b
+
+    def get_perimetr(self):
+        return 2 * (self.a + self.b)
+
+    def __repr__(self):
+        return f"Square({self.a}, {self.b})"
+
+
+class Other:
+    pass
+
+
+class TestRect(unittest.TestCase):
+    def test_value_error(self):
+        with self.assertRaises(ValueError):
+            Rect(0, 5)
+
+    def test_type_error_add(self):
+        with self.assertRaises(TypeError):
+            Rect(4, 5) + Other
+
+    def test_type_error_sub(self):
+        with self.assertRaises(TypeError):
+            Rect(4, 5) - Other
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_fail_rect(self, mock_stdout):
+        with self.assertRaises(ValueError):
+            Rect(1, 1) - Rect(5, 5)
+            self.assertEquals(mock_stdout.getvalue(), 'Такой прямоугольник невозможен')
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
--- Lesson_14/lesson14_task6.py
+++ Lesson_14/lesson14_task6.py
@@ -2,6 +2,124 @@
 # Напишите 3-7 тестов pytest для данного проекта.
 # Используйте фикстуры.
 
+import json
+import os.path
+
+import pytest
 
 
+class UserException(Exception):
+    pass
 
+
+class UserLevelError(UserException):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f'Ошибка уровня! {self.value}'
+
+
+class UserPermissionError(UserException):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f'Ошибка доступа! {self.value}'
+
+
+class Person:
+
+    def __init__(self, firstname, lastname, sex, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.sex = sex
+        self.__age = age
+
+    def birthday(self):
+        self.__age += 1
+
+    def full_name(self):
+        return f'{self.firstname} {self.lastname} {self.sex}'
+
+    def get_age(self):
+        return self.__age
+
+
+class Employee(Person):
+    def __init__(self, firstname, lastname, sex, age, pers_id):
+        if len(pers_id) != 6:
+            raise ValueError('Некорректный id!')
+        super().__init__(firstname, lastname, sex, age)
+        self.pers_id = pers_id
+        self.lvl_id = int(pers_id) % 7
+        self.json_data = {self.firstname: [lastname, sex, age, self.lvl_id, pers_id]}
+        self.save_in_files()
+
+    def __str__(self):
+        return f'{self.firstname}: уровень: {self.lvl_id}, ID: {self.pers_id}'
+
+    def save_in_files(self):
+        try:
+            if not os.path.isfile('json_file.json') or os.path.getsize('json_file.json') == 0:
+                with open('json_file.json', 'w') as f:
+                    f.write('{}')
+            with open('json_file.json', 'r') as f_read:
+                data = json.load(f_read)
+                data.update(self.json_data)
+            with open('json_file.json', 'w', encoding='utf-8') as f_write:
+                json.dump(data, f_write, ensure_ascii=False, indent=2)
+        except SystemError as e:
+            return f'Ошибка записи! Не удалось открыть файл.'
+
+    def __eq__(self, other):
+        if isinstance(other, Employee):
+            return self.pers_id == other.pers_id
+        return NotImplemented
+
+
+def user_create():
+    user_list = []
+    with open('json_file.json', 'r') as f_read:
+        data = json.load(f_read)
+    for firstname, values in data.items():
+        lastname, sex, age, lvl_id, pers_id = values
+        user_list.append(Employee(firstname, lastname, sex, age, pers_id))
+    return user_list
+
+
+class Login:
+    def user_login(self, user_name, user_id):
+        with open('json_file.json', 'r') as f_read:
+            data = json.load(f_read)
+            if user_name in data and data[user_name][4] == user_id:
+                return int(data[user_name][3])
+            else:
+                raise UserPermissionError(user_id)
+
+    def user_creator(self, data, creator_lvl):
+        new_user = Employee(*data)
+        if new_user.lvl_id <= creator_lvl:
+            return new_user
+        else:
+            raise UserLevelError(creator_lvl)
+
+@pytest.fixture
+def data():
+    e4 = ('Антон', 'Анатольевич', 'М', 24, '123456')
+    us1 = Login()
+    return [e4, us1]
+
+
+def test_value_error():
+    assert Employee('Вася', 'Иванов', 'М', 30, '123456')
+
+def test_user_lvl_err(data):
+    assert data[1].user_creator(data[0], data[1].user_login('Антон', '123456'))
+
+def test_user_perm_err(data):
+    assert data[1].user_login('Антон', '123456')
+
+
+if __name__ == "__main__":
+    pytest.main()
