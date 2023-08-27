7c9f0afd4342893ebd9890c0d14648792a3dd1fb
PavelSB <1987PavelS@mail.ru>
2023-08-28 00:17:11 +0300

    lesson 13 task 6 done

--- Lesson_13/lesson13_task6.py
+++ Lesson_13/lesson13_task6.py
@@ -1,5 +1,5 @@
 # Доработайте классы исключения так, чтобы они выдали подробную информацию об ошибках.
 # Передавайте необходимые данные из основного кода проекта.
 
-
-
+print('task completed, check here: \n'
+      'https://github.com/PavelSBakhtin/Python-Lessons/blob/master/Lesson_13/lesson13_task5.py')
